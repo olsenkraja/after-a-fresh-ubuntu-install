df1cc862171a680e667c2c92d7b63fd90a3746e9
Olsen <krajaolsen@gmail.com>
2020-04-21 01:02:44 +0200

    Fixed Chrome path

--- after-a-fresh-ubuntu-install.sh
+++ after-a-fresh-ubuntu-install.sh
@@ -1,9 +1,9 @@
 # Chrome
 sudo apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/google-chrome.deb
-sudo dpkg -i google-chrome*.deb
+sudo dpkg -i ~/google-chrome.deb
 sudo apt-get install -f
-rm google-chrome.deb
+rm ~/google-chrome.deb
 
 # VS Code
 sudo snap install code --classic
