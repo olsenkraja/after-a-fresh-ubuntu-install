44e418c914752e31a0236116e4ab579ea88eccce
Olsen <krajaolsen@gmail.com>
2020-04-21 01:00:14 +0200

    Added CopyQ

--- after-a-fresh-ubuntu-install.sh
+++ after-a-fresh-ubuntu-install.sh
@@ -1,9 +1,9 @@
 # Chrome
 sudo apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/google-chrome.deb
-sudo apt install ./google-chrome*.deb
+sudo dpkg -i google-chrome*.deb
 sudo apt-get install -f
-rm ./google-chrome.deb
+rm google-chrome.deb
 
 # VS Code
 sudo snap install code --classic
@@ -32,3 +32,8 @@ sudo snap install phpstorm --classic
 
 # Tilix
 sudo apt install tilix
+
+# CopyQ
+sudo add-apt-repository ppa:hluk/copyq
+sudo apt update
+sudo apt install copyq
