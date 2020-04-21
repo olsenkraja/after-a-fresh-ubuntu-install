ecf37ecd5fa7d191b05e2f665e3620d513d82b1a
Olsen <krajaolsen@gmail.com>
2020-04-21 15:02:39 +0200

    Add special characters to the US Euro 5 layout

--- after-a-fresh-ubuntu-install.sh
+++ after-a-fresh-ubuntu-install.sh
@@ -1,3 +1,6 @@
+# Add special characters to the US Euro 5 layout
+sudo cp eurosign /usr/share/X11/xkb/symbols/eurosign
+
 # Chrome
 sudo apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/google-chrome.deb
